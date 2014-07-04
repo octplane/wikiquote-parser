b215d25df5608b973c38ab0819200e19d72288ff
Pierre Baillet <oct@zoy.org>
2014-07-04 17:36:12 +0200

    Start working on an actual Parser

--- parser.go
+++ parser.go
@@ -9,6 +9,7 @@ import (
   "regexp"
   "sort"
   "strings"
+  "unicode/utf8"
 )
 
 type Command struct {
@@ -82,7 +83,347 @@ func sortMapByValue(m map[string]int) PairList {
   return p
 }
 
+/// LEXER
+
+type item struct {
+  typ itemType
+  val string
+}
+
+type itemType int
+
+const (
+  itemError = iota
+  itemTemplate
+  itemTemplateName
+  itemText
+  itemEOF
+)
+
+type sRune struct {
+  typ runeType
+  val rune
+}
+
+type runeType int
+
+const (
+  eof = iota
+  ruune
+)
+
+func (i item) String() string {
+  switch i.typ {
+  case itemEOF:
+    return "EOF"
+  case itemError:
+    return i.val
+  case itemTemplateName:
+    return fmt.Sprintf("t: \"%s\"", i.val)
+  case itemTemplate:
+    return fmt.Sprintf("template %s\n", i.val)
+  case itemText:
+    if len(i.val) > 10 {
+      return fmt.Sprintf("%.10q...\n", i.val)
+    }
+    return fmt.Sprintf("%q\n", i.val)
+  }
+  return fmt.Sprintf("Unknown item %+v\n", i.val)
+}
+
+// stateFn represents the state of the scanner
+// as a function that returns the next state.
+type stateFn func(*lexer) stateFn
+
+// lexer holds the state of the scanner.
+type lexer struct {
+  name  string    // used only for error reports.
+  input string    // the string being scanned.
+  start int       // start position of this item.
+  pos   int       // current position in the input.
+  width int       // width of last rune read from input.
+  items chan item // channel of scanned items.
+  state stateFn
+}
+
+// emit passes an item back to the client.
+func (l *lexer) emit(t itemType) {
+  l.items <- item{t, l.input[l.start:l.pos]}
+  l.start = l.pos
+}
+
+// run lexes the input by executing state functions until
+// the state is nil.
+func (l *lexer) run() {
+  for state := lexText; state != nil; {
+    state = state(l)
+  }
+  close(l.items) // No more tokens will be delivered.
+}
+
+// next returns the next rune in the input.
+func (l *lexer) next() (r sRune) {
+  var ru rune
+  if l.pos >= len(l.input) {
+    l.width = 0
+    return sRune{typ: eof}
+  }
+  ru, l.width =
+    utf8.DecodeRuneInString(l.input[l.pos:])
+  l.pos += l.width
+  return sRune{val: ru, typ: ruune}
+}
+
+// ignore skips over the pending input before this point.
+func (l *lexer) ignore() {
+  l.start = l.pos
+}
+
+// backup steps back one rune.
+// Can be called only once per call of next.
+func (l *lexer) backup() {
+  l.pos -= l.width
+}
+
+// peek returns but does not consume
+// the next rune in the input.
+func (l *lexer) peek() sRune {
+  r := l.next()
+  l.backup()
+  return r
+}
+
+// accept consumes the next rune
+// if it's from the valid set.
+func (l *lexer) accept(valid string) bool {
+  if strings.IndexRune(valid, l.next().val) >= 0 {
+    return true
+  }
+  l.backup()
+  return false
+}
+
+// acceptRun consumes a run of runes from the valid set.
+func (l *lexer) acceptRun(valid string) {
+  for strings.IndexRune(valid, l.next().val) >= 0 {
+  }
+  l.backup()
+}
+
+func (l *lexer) refuse(valid string) {
+  for strings.IndexRune(valid, l.next().val) == -1 {
+  }
+  l.backup()
+}
+
+// error returns an error token and terminates the scan
+// by passing back a nil pointer that will be the next
+// state, terminating l.run.
+func (l *lexer) errorf(format string, args ...interface{}) stateFn {
+  l.items <- item{
+    itemError,
+    fmt.Sprintf(format, args...),
+  }
+  return nil
+}
+
+// func lexNumber(l *lexer) stateFn {
+//   // Optional leading sign.
+//   l.accept("+-")
+//   // Is it hex?
+//   digits := "0123456789"
+//   if l.accept("0") && l.accept("xX") {
+//     digits = "0123456789abcdefABCDEF"
+//   }
+//   l.acceptRun(digits)
+//   if l.accept(".") {
+//     l.acceptRun(digits)
+//   }
+//   if l.accept("eE") {
+//     l.accept("+-")
+//     l.acceptRun("0123456789")
+//   }
+//   // Is it imaginary?
+//   l.accept("i")
+//   // Next thing mustn't be alphanumeric.
+//   if isAlphaNumeric(l.peek()) {
+//     l.next()
+//     return l.errorf("bad number syntax: %q",
+//       l.input[l.start:l.pos])
+//   }
+//   l.emit(itemNumber)
+//   return lexInsideAction
+// }
+
+// lex creates a new scanner for the input string.
+func lex(name, input string) *lexer {
+  l := &lexer{
+    name:  name,
+    input: input,
+    state: lexText,
+    items: make(chan item, 2), // Two items sufficient.
+  }
+  return l
+}
+
+// nextItem returns the next item from the input.
+func (l *lexer) nextItem() item {
+  for {
+    select {
+    case item := <-l.items:
+      return item
+    default:
+      l.state = l.state(l)
+    }
+  }
+  panic("not reached")
+}
+
+const leftTemplate = "{{"
+const rightTemplate = "}}"
+
+func lexText(l *lexer) stateFn {
+  for {
+    if strings.HasPrefix(l.input[l.pos:], leftTemplate) {
+      if l.pos > l.start {
+        l.pos += 2
+        l.emit(itemText)
+      }
+      return lexTemplateName // Next state.
+    }
+    if l.next().typ == eof {
+      break
+    }
+  }
+  // Correctly reached EOF.
+  if l.pos > l.start {
+    l.emit(itemText)
+  }
+  l.emit(itemEOF) // Useful to make EOF a token.
+  return nil      // Stop the run loop.
+}
+
+func lexLeftTemplate(l *lexer) stateFn {
+  for {
+    if strings.HasPrefix(l.input[l.pos:], rightTemplate) {
+      if l.pos > l.start {
+        l.pos += 2
+        l.emit(itemTemplate)
+      }
+      return lexText // Next state.
+    }
+    if l.next().typ == eof {
+      break
+    }
+  }
+  // Correctly reached EOF.
+  if l.pos > l.start {
+    l.emit(itemText)
+  }
+  l.emit(itemEOF) // Useful to make EOF a token.
+  return nil      // Stop the run loop.
+}
+
+func lexTemplateName(l *lexer) stateFn {
+  l.refuse("|")
+  l.emit(itemTemplateName)
+  return lexLeftTemplate
+}
+
+// const leftMeta = "{{"
+
+// func lexText(l *lexer) stateFn {
+//   for {
+//     if strings.HasPrefix(l.input[l.pos:], leftMeta) {
+//       if l.pos > l.start {
+//         l.emit(itemText)
+//       }
+//       return lexLeftMeta // Next state.
+//     }
+//     if l.next().typ == eof {
+//       break
+//     }
+//   }
+//   // Correctly reached EOF.
+//   if l.pos > l.start {
+//     l.emit(itemText)
+//   }
+//   l.emit(itemEOF) // Useful to make EOF a token.
+//   return nil      // Stop the run loop.
+// }
+
+// func lexLeftMeta(l *lexer) stateFn {
+//   l.pos += len(leftMeta)
+//   l.emit(itemLeftMeta)
+//   return lexInsideAction // Now inside {{ }}.
+// }
+
+// func lexInsideAction(l *lexer) stateFn {
+//   // Either number, quoted string, or identifier.
+//   // Spaces separate and are ignored.
+//   // Pipe symbols separate and are emitted.
+//   for {
+//     if strings.HasPrefix(l.input[l.pos:], rightMeta) {
+//       return lexRightMeta
+//     }
+//     switch r := l.next(); {
+//     case r == eof || r == '\n':
+//       return l.errorf("unclosed action")
+//     case isSpace(r):
+//       l.ignore()
+//     case r == '|':
+//       l.emit(itemPipe)
+//     case r == '"':
+//       return lexQuote
+//     case r == '`':
+//       return lexRawQuote
+//     case r == '+' || r == '-' || '0' <= r && r <= '9':
+//       l.backup()
+//       return lexNumber
+//     case isAlphaNumeric(r):
+//       l.backup()
+//       return lexIdentifier
+//     }
+//   }
+// }
+// func lexNumber(l *lexer) stateFn {
+//     // Optional leading sign.
+//     l.accept("+-")
+//     // Is it hex?
+//     digits := "0123456789"
+//     if l.accept("0") && l.accept("xX") {
+//         digits = "0123456789abcdefABCDEF"
+//     }
+//     l.acceptRun(digits)
+//     if l.accept(".") {
+//         l.acceptRun(digits)
+//     }
+//     if l.accept("eE") {
+//         l.accept("+-")
+//         l.acceptRun("0123456789")
+//     }
+
+////
+
 func markupExtractor(title string, body string) []Command {
+
+  l := lex(title, body)
+  go l.run()
+
+  var it item
+  halt := false
+  for !halt {
+    it = l.nextItem()
+    fmt.Printf(it.String())
+    switch it.typ {
+    case itemEOF:
+      halt = true
+      fmt.Println("EOF !!")
+      break
+    }
+  }
+  panic("Hammer time!")
+
   markup := regexp.MustCompile("(?s){{[^}]+}}")
   param := regexp.MustCompile("([^=]+)=(.*)")
 
@@ -126,6 +467,7 @@ func markupExtractor(title string, body string) []Command {
     // Ignore the empty command
     if cmd != "" {
       commands[pos] = Command{Cmd: cmd, PageTitle: title, Arguments: arguments[1:], NamedArguments: kvArguments}
+      fmt.Printf("%+v\n", commands[pos])
       pos += 1
     }
   }
@@ -235,8 +577,8 @@ func main() {
   textXPath := xmlpath.MustCompile(("revision/text"))
   titleXPath := xmlpath.MustCompile("title")
 
-  fi, err := os.Open("frwikiquote-20140622-pages-articles-multistream.xml")
-  // fi, err := os.Open("sample.xml")
+  //fi, err := os.Open("frwikiquote-20140622-pages-articles-multistream.xml")
+  fi, err := os.Open("sample.xml")
 
   if err != nil {
 
