2b1756acbf6c8020fabccbb9a1c075df89045846
Pierre Baillet <oct@zoy.org>
2014-07-10 18:01:38 +0200

    Another sample

--- quote_extractor/main.go
+++ quote_extractor/main.go
@@ -185,8 +185,8 @@ func main() {
   textXPath := xmlpath.MustCompile(("revision/text"))
   titleXPath := xmlpath.MustCompile("title")
 
-  //fi, err := os.Open("frwikiquote-20140622-pages-articles-multistream.xml")
-  fi, err := os.Open("sample.xml")
+  // fi, err := os.Open("frwikiquote-20140622-pages-articles-multistream.xml")
+  fi, err := os.Open("sample1.xml")
 
   if err != nil {
 
